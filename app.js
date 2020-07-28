4cf0377eafc523b0b251e8a9bca73cd047259601
ca3rot <ca3rot@gmail.com>
2020-07-28 12:46:27 +0900

    Edited some conflicts

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const admin = require('./routes/admin');
+var methodOverride = require('method-override');
 const app = express();
+
+const admin = require('./routes/admin');
+const contacts = require('./routes/contacts');
+
 const nunjucks = require('nunjucks');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+
 const db = require('./models');
 const Products = require('./models/Products');
+const Contacts = require('./models/Contacts');
 const port = 8080;
 
 nunjucks.configure('templates', {
@@ -17,7 +23,7 @@ nunjucks.configure('templates', {
 db.sequelize.authenticate()
     .then(() => {
         console.log('Connection has been established successfully');
-        // return db.sequelize.sync();
+        return db.sequelize.sync();
     })
     .then(() => {
         console.log('DB sync complete');
@@ -27,18 +33,16 @@ db.sequelize.authenticate()
     });
 
 
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
-
-
+app.use(methodOverride('_method'))
 
 app.get('/', function(req, res){
     res.send('처음 만들어봐요...');
 });
 app.use('/admin', admin);
-app.use('/contract', contract);
+app.use('/contacts', contacts);
 
 
 app.listen(port, function(){
